eac3d2e064b0770a2dd168e54cbe6af7c0e3e873
boringhexi <boringhexi@pm.me>
2024-01-25 23:13:50 -0500

    version bump

--- addons/io_scene_ghs_mappm2/__init__.py
+++ addons/io_scene_ghs_mappm2/__init__.py
@@ -5,7 +5,7 @@ from bpy_extras.io_utils import ImportHelper
 bl_info = {
     "name": "Gregory Horror Show GHS/MAP-PM2 format",
     "author": "boringhexi",
-    "version": (0, 1, 4),
+    "version": (0, 1, 5),
     "blender": (4, 0, 0),
     "location": "File > Import",
     "description": "For .ghs and .map-pm2 files from Gregory Horror Show (PS2)",
